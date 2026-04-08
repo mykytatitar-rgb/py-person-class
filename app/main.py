0df5d400cbf33c1e0cbae405815d564655669b1a
Mykyta Titar <mykyta.titar@gmail.com>
2026-04-08 09:58:26 +0300

    Solution2

--- app/main.py
+++ app/main.py
@@ -9,14 +9,11 @@ class Person:
 
 
 def create_person_list(people: list) -> list:
-    list_people = []
-    for person_data in people:
-        person = Person(name=person_data["name"], age=person_data["age"])
+    list_people = [Person(name=person_data["name"], age=person_data["age"]) for person_data in people]
     for person_data in people:
         person = Person.people[person_data["name"]]
         if person_data.get("wife"):
             person.wife = Person.people[person_data["wife"]]
         if person_data.get("husband"):
             person.husband = Person.people[person_data["husband"]]
-        list_people.append(person)
     return list_people
